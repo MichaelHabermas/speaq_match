1896d2e5802df57ec537cbefbad46c8b1dea1ae4
MichaelHabermas <michaelghabermas@gmail.com>
2021-11-03 15:13:06 -0400

    game over screen

--- app/screens/GameOverScreen.js
+++ app/screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 // redux
 import { connect } from "react-redux";
@@ -7,10 +7,22 @@ import { connect } from "react-redux";
 // components
 import NavButtonSection from "../components/NavButtonSection";
 import Screen from "../components/Screen";
+import Text from "../components/Text";
 
-function GameOverScreen({ navigation }) {
+function GameOverScreen({
+	navigation,
+	currentDeckName,
+	currentLevel,
+	languageToLearn,
+}) {
 	return (
 		<Screen style={styles.screen} screen={true}>
+			<View style={styles.gameOverTextContainer}>
+				<Text style={styles.gameOverText}>Level {currentLevel} Complete!</Text>
+				<Text style={styles.gameOverText}>Language: {languageToLearn}</Text>
+				<Text style={styles.gameOverText}>Deck: {currentDeckName}</Text>
+				<Text style={styles.gameOverText}>Next Level: {currentLevel + 1}</Text>
+			</View>
 			<NavButtonSection navigation={navigation} />
 		</Screen>
 	);
@@ -18,10 +30,20 @@ function GameOverScreen({ navigation }) {
 
 const styles = StyleSheet.create({
 	screen: {},
+	gameOverTextContainer: {
+		display: "flex",
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	gameOverText: { fontSize: 35, marginVertical: 20 },
 });
 
 const mapStateToProps = state => ({
-	gameState: state.matchAndMemory,
+	currentDeckName: state.matchAndMemory.gameSettings.currentDeckName,
+	currentLevel: state.matchAndMemory.gameSettings.currentLevel,
+	languageToLearn: state.matchAndMemory.gameSettings.languageToLearn,
 });
 
 export default connect(mapStateToProps)(GameOverScreen);
